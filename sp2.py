2a529adbd78f9b4962bbeca1ac30c8965119f99f
u03013112@hotmail.com <u03013112@hotmail.com>
2020-09-01 19:30:43 +0800

    白嫖

--- sp2.py
+++ sp2.py
@@ -1,14 +1,14 @@
 # encoding:utf-8
 import requests
 
-class SP1:
-    def __init__(self,comic_id):
-        print('爬取一本书目录的类')
-        self.url = 'http://j.jinkongjianshe.com/api/comic/comic_detail/?comic_id=%d'%(comic_id)
+class SP2:
+    def __init__(self,comic_id,chapter_id):
+        print('爬取一本书内容的类')
+        self.url = 'http://j.jinkongjianshe.com/api/comic/read_chapter?comic_id=%d&chapter_id=%d' % (comic_id,chapter_id)
     def sp(self):
         try:
             r = requests.get(self.url)
-            data = r.json()['data']['chapter_list']
+            data = r.json()['data']['images']
             # print(data)
             self.data = data
             return data
@@ -16,5 +16,5 @@ class SP1:
             print(e)
 
 if __name__=='__main__':  
-    s = SP1(234)
+    s = SP2(234,8415)
     s.sp()
