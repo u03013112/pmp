764a1673d914758f39c0dfe906f486b6462029d0
u03013112@hotmail.com <u03013112@hotmail.com>
2020-09-01 19:40:08 +0800

    白嫖主要内容

---
+++ sign.py
@@ -0,0 +1,23 @@
+# encoding:utf-8
+import requests
+
+class Sign:
+    def __init__(self,ua):
+        print('爬取签到的类')
+        self.url = 'http://j.jinkongjianshe.com/api/user/sign_in'
+        self.ua = ua
+    def sp(self):
+        try:
+            headers = {'User-Agent': self.ua}
+            r = requests.post(self.url,headers=headers)
+            data = r.json()['code']
+            # data = r.text
+            print(data)
+            # self.data = data
+            return data
+        except requests.exceptions.RequestException as e:
+            print(e)
+
+if __name__=='__main__':  
+    s = Sign("1a")
+    s.sp()
