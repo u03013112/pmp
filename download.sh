0bebea195fcddba494b10808e0e83871ee179ece
u03013112@hotmail.com <u03013112@hotmail.com>
2020-09-02 19:29:03 +0800

    下载脚本添加参数，可以稍微控制一下，太慢了

--- download.sh
+++ download.sh
@@ -1,7 +1,15 @@
 #/bin/bash
+start=0
+end=1000
+count=1000
+if [ $# -eq 2 ];then
+    start=$1
+    end=$2
+    count=`expr ${end} - ${start}`
+fi
 mkdir -p download
 script_path=script
-scripts=`ls ${script_path}`
+scripts=`ls ${script_path}|head -n ${end}|tail -n ${count}`
 cd download
 for file in $scripts:
 do
